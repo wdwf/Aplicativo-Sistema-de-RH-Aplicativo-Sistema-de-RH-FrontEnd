f7d3ba0c1740f541d5d0e192e9503511058975cd
Bowie <larissa.soaress@yahoo.com.br>
2025-06-06 13:04:37 -0300

    Alterações

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,64 @@
+import { createContext, type ReactNode, useState } from "react"
+
+import type UsuarioLogin from "../models/UsuarioLogin"
+import { login } from "../services/Service"
+import { ToastAlerta } from "../utils/ToastAlerta"
+
+
+interface AuthContextProps {
+    usuario: UsuarioLogin
+    setUsuario: React.Dispatch<React.SetStateAction<UsuarioLogin>>
+    handleLogout(): void
+    handleLogin(usuario: UsuarioLogin): Promise<void>
+    isLoading: boolean
+}
+
+interface AuthProviderProps {
+    children: ReactNode
+}
+
+export const AuthContext = createContext({} as AuthContextProps)
+
+export function AuthProvider({ children }: AuthProviderProps) {
+
+    const [usuario, setUsuario] = useState<UsuarioLogin>({
+        id: 0,
+        nome: "",
+        usuario: "",
+        senha: "",
+        foto: "",
+        tipo: "",
+        token: ""
+    })
+
+    const [isLoading, setIsLoading] = useState(false)
+
+    async function handleLogin(usuarioLogin: UsuarioLogin) {
+        setIsLoading(true)
+        try {
+            await login(`/usuarios/logar`, usuarioLogin, setUsuario)
+            ToastAlerta("Usuário foi autenticado com sucesso!", "sucesso")
+        } catch (error) {
+            ToastAlerta("Os dados do Usuário estão inconsistentes!", "erro")
+        }
+        setIsLoading(false)
+    }
+
+    function handleLogout() {
+        setUsuario({
+            id: 0,
+            nome: "",
+            usuario: "",
+            senha: "",
+            foto: "",
+            tipo: "",
+            token: ""
+        })
+    }
+
+    return (
+        <AuthContext.Provider value={{ usuario, setUsuario, handleLogin, handleLogout, isLoading }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
--- src/models/UsuarioLogin.ts
+++ src/models/UsuarioLogin.ts
@@ -4,5 +4,6 @@ export default interface UsuarioLogin {
     usuario: string;
     senha: string;
     foto: string;
+    tipo: string;
     token: string;
 }
