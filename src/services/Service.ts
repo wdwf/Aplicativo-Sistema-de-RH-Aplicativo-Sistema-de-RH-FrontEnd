f4407dfbda5be963638134551dc1090f891c529a
Bowie <larissa.soaress@yahoo.com.br>
2025-06-07 15:34:15 -0300

    Adciona a rota de calcular salario

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
+    baseURL:" https://aplicativo-sistema-rh.onrender.com"
 })
 
 export const cadastrarUsuario = async (url: string, dados: Object, setDados: Function) => {
@@ -31,4 +31,11 @@ export const atualizar = async (url: string, dados: Object, setDados: Function, 
 
 export const deletar = async (url: string, header: Object) => {
     await api.delete(url, header)
+}
+
+export const calcularSalario = async (usuarioId: number, horasTrabalhadas: number, bonus: number, descontos: number, setDados: Function, header: Object) => {
+  const resposta = await api.get(`/usuario/${usuarioId}/calcular-salario`, {
+    params: {horasTrabalhadas:horasTrabalhadas ,bonus:bonus ,descontos:descontos },
+    ...header})
+  setDados(resposta.data)
 }
