cad4ecbb37af201208e1cbcd0469f2c49926a798
Bowie <larissa.soaress@yahoo.com.br>
2025-06-07 17:01:20 -0300

    Correções

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,8 @@
-<<<<<<< HEAD
-=======
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
->>>>>>> 6b6b272ce4306629e5fef93482340adc066b1c47
 import './App.css'
 import Footer from './components/Footer'
-import Navbar from './components/Navbar'
 import Home from './pages/home/Home'
-import { AuthProvider } from './contexts/AuthContext'
-
-import 'react-toastify/dist/ReactToastify.css';
-import { ToastContainer } from 'react-toastify'
-
-
 import Navbar from './components/Navbar'
-
 import { AuthProvider } from './contexts/AuthContext'
-
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify'
 import Login from './pages/Login'
@@ -27,7 +14,6 @@ function App() {
  
 
   return (
-    <>
        <AuthProvider>
         <ToastContainer />
           <BrowserRouter>
@@ -42,9 +28,7 @@ function App() {
           </div>
           <Footer />
         </BrowserRouter>
-        
         </AuthProvider> 
-    </>
   )
 }
 
