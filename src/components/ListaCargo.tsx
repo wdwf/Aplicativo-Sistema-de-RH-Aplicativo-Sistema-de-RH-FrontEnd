d738a5e38c290ed75fd443d9770812e9323e6fa4
wdwf <w_dwf@hotmail.com>
2025-06-08 03:50:52 -0300

    fix: correçao das funcionalidades de gerenciamento dos cargos

--- src/components/ListaCargo.tsx
+++ src/components/ListaCargo.tsx
@@ -5,11 +5,13 @@ import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../contexts/AuthContext";
 import { buscar } from "../services/Service";
 import { ToastAlerta } from "../utils/ToastAlerta";
+import { Hourglass, RotatingLines } from "react-loader-spinner";
 
 export default function ListaCargo() {
   const navigate = useNavigate();
 
   const [listaCargos, setListaCargos] = useState<Cargo[]>([])
+  const [loadingPage, setLoadingPage] = useState(true);
 
   const { usuario, handleLogout } = useContext(AuthContext)
   const token = usuario.token
@@ -19,31 +21,54 @@ export default function ListaCargo() {
       await buscar('/cargo', setListaCargos, {
         headers: { Authorization: token }
       })
+      setLoadingPage(false);
     } catch (error: any) {
       if (error.toString().includes('403')) {
-        handleLogout()
+        // handleLogout()
+        console.log(error);
+
       }
     }
   }
 
   useEffect(() => {
-    if (token === '') {
+    console.log('token', token);
+
+    if (token === '') return;
+    if (!token) {
       ToastAlerta('Você precisa estar logado!', 'info')
+      handleLogout();
       navigate('/')
+    }
+    else {
+      buscarCargos()
     }
   }, [token])
 
-  useEffect(() => {
-    buscarCargos()
-  }, [listaCargos.length])
+  if (loadingPage) {
+    return <Hourglass
+      visible={true}
+      height="80"
+      width="80"
+      ariaLabel="hourglass-loading"
+      wrapperStyle={{}}
+      wrapperClass=""
+      colors={['#306cce', '#72a1ed']}
+    />
+  }
 
   return (
-    <>
-      {
-        listaCargos.map((cargo) => (
-          <CardCargo key={cargo.id} cargo={cargo} />
-        ))
-      }
-    </>
+    <div className="flex justify-center w-full my-4">
+      <div className="container flex flex-col">
+        <div className="grid grid-cols-1 md:grid-cols-2 
+                                    lg:grid-cols-3 gap-8">
+          {
+            listaCargos.map((cargo) => (
+              <CardCargo key={cargo.id} cargo={cargo} />
+            ))
+          }
+        </div>
+      </div>
+    </div>
   )
 }
