380f63cb66fbd3ba1a9bf173bf348fb3e3fa0e31
eblopes23 <eblopes23@gmail.com>
2025-06-24 21:45:18 -0300

    Criação de busca rápida e filtros para procura de colaboradores

--- src/components/cargo/ListaCargo.tsx
+++ src/components/cargo/ListaCargo.tsx
@@ -7,12 +7,16 @@ import { ToastAlerta } from "../../utils/ToastAlerta";
 import { Hourglass } from "react-loader-spinner";
 import CardCargo from "./CardCargo";
 import { IoArrowBackSharp } from "react-icons/io5";
+import { Search } from "lucide-react";
 
 export default function ListaCargo() {
 
   const navigate = useNavigate();
   window.scrollTo(0, 0);
+  const [cargo, setCargo] = useState<Cargo[]>([]);
   const [listaCargos, setListaCargos] = useState<Cargo[]>([])
+  const [cargosFiltrados, setCargosFiltrados] = useState<Cargo[]>([]);
+  const [busca, setBusca] = useState('');
   const [loadingPage, setLoadingPage] = useState(true);
 
   const { usuario, handleLogout } = useContext(AuthContext)
@@ -20,9 +24,12 @@ export default function ListaCargo() {
 
   async function buscarCargos() {
     try {
-      await buscar('/cargo', setListaCargos, {
+       await buscar("/cargo", (res: Cargo[]) => {
+              setCargo(res);
+              setCargosFiltrados(res);
+            },  {
         headers: { Authorization: token }
-      })
+      });
       setLoadingPage(false);
     } catch (error: any) {
       if (error.toString().includes('403')) {
@@ -47,6 +54,13 @@ export default function ListaCargo() {
     }
   }, [token])
 
+ useEffect(() => {
+    const resultado = cargo.filter(dep =>
+      dep.nome.toLowerCase().includes(busca.toLowerCase())
+    );
+    setCargosFiltrados(resultado);
+  }, [busca, cargo]);
+
   if (loadingPage) {
     return <Hourglass
       visible={true}
@@ -61,26 +75,44 @@ export default function ListaCargo() {
 
   return (
     <div className="w-full p-6">
-      <div className="flex gap-3 ml-1 my-6 items-center">
-        <Link to="/home" className="flex items-center rounded-full gap-2 mb-6 hover:bg-gray-200 p-4 hover:-translate-x-2 transition-all duration-300">
-          <IoArrowBackSharp className="w-7 h-7" />
-        </Link>
-        <h3 className="text-3xl font-medium text-rh-primarygrey mb-6">
-          Lista de Cargos
-        </h3>
+      <div className="flex justify-between items-center flex-wrap gap-4 mb-6">
+        <div className="flex gap-3 ml-1 items-center">
+          <Link to="/home" className="flex items-center rounded-full gap-2 hover:bg-gray-200 p-4 hover:-translate-x-2 transition-all duration-300">
+            <IoArrowBackSharp className="w-7 h-7 text-rh-primarygrey" />
+          </Link>
+          <h3 className="text-3xl font-medium text-rh-primarygrey">
+            Lista de Cargos
+          </h3>
+        </div>
+
+        <div className="flex items-center px-3 py-1.5 border border-gray-600 rounded-lg text-sm w-72 gap-3">
+          
+          <input
+            type="text"
+            placeholder="Buscar departamento"
+            className="bg-transparent flex-1 outline-none border-0 p-0 text-sm focus:ring-0"
+            onChange={(e) => setBusca(e.target.value)}
+            autoComplete="off"
+            value={busca}
+          />
+          <Search className="size-4 text-rh-primarygrey" />
+        </div>
       </div>
+
       <div className="flex flex-wrap gap-4">
         {
-          listaCargos.length === 0 && (
+          cargosFiltrados.length === 0 ? (
             <div className="flex flex-col items-center justify-center w-full h-full">
-              <p className="text-xl font-semibold text-rh-primarygrey mt-4">Nenhum cargo cadastrado.</p>
+              <p className="text-xl font-semibold text-rh-primarygrey mt-4">Nenhum cargo encontrado.</p>
             </div>
+          ) : (
+            cargosFiltrados.map((cargo) => (
+              <CardCargo
+                key={cargo.id}
+                cargo={cargo}
+              />
+            ))
           )
-        }
-        {
-          listaCargos.map((cargo) => (
-            <CardCargo key={cargo.id} cargo={cargo} />
-          ))
         }
       </div>
     </div>
--- src/components/departamento/ListaDepartamento.tsx
+++ src/components/departamento/ListaDepartamento.tsx
@@ -7,22 +7,26 @@ import { ToastAlerta } from "../../utils/ToastAlerta";
 import { RotatingLines } from "react-loader-spinner";
 import CardDepartamento from "./CardDepartamento";
 import { IoArrowBackSharp } from "react-icons/io5";
-
+import { Search } from "lucide-react"; // Ícone da lupa
 
 function ListaDepartamento() {
-
   const navigate = useNavigate();
   window.scrollTo(0, 0);
+
   const [departamento, setDepartamento] = useState<Departamento[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [isLoadinPage, setIsLoadingPage] = useState<boolean>(true);
+  const [departamentosFiltrados, setDepartamentosFiltrados] = useState<Departamento[]>([]);
+  const [busca, setBusca] = useState('');
+  const [isLoadingPage, setIsLoadingPage] = useState<boolean>(true);
 
   const { usuario, handleLogout } = useContext(AuthContext);
   const token = usuario.token;
 
   async function buscarDepartamento() {
     try {
-      await buscar("/departamento", setDepartamento, {
+      await buscar("/departamento", (res: Departamento[]) => {
+        setDepartamento(res);
+        setDepartamentosFiltrados(res);
+      }, {
         headers: { Authorization: token },
       });
       setIsLoadingPage(false);
@@ -34,9 +38,8 @@ function ListaDepartamento() {
   }
 
   useEffect(() => {
-    if (token === "") {
-      return;
-    }
+    if (token === "") return;
+
     if (!token) {
       ToastAlerta("Você precisa estar Logado!", "info");
       handleLogout();
@@ -46,7 +49,14 @@ function ListaDepartamento() {
     }
   }, [token]);
 
-  if (isLoadinPage) {
+  useEffect(() => {
+    const resultado = departamento.filter(dep =>
+      dep.nome.toLowerCase().includes(busca.toLowerCase())
+    );
+    setDepartamentosFiltrados(resultado);
+  }, [busca, departamento]);
+
+  if (isLoadingPage) {
     return (
       <RotatingLines
         strokeColor="black"
@@ -58,32 +68,47 @@ function ListaDepartamento() {
     );
   }
 
-
   return (
-
     <div className="w-full p-6">
-      <div className="flex gap-3 ml-1 my-6 items-center">
-        <Link to="/home" className="flex items-center rounded-full gap-2 mb-6 hover:bg-gray-200 p-4 hover:-translate-x-2 transition-all duration-300">
-          <IoArrowBackSharp className="w-7 h-7 text-rh-primarygrey" />
-        </Link>
-        <h3 className="text-3xl font-medium text-rh-primarygrey mb-6">
-          Lista de Departamentos
-        </h3>
+      <div className="flex justify-between items-center flex-wrap gap-4 mb-6">
+        <div className="flex gap-3 ml-1 items-center">
+          <Link to="/home" className="flex items-center rounded-full gap-2 hover:bg-gray-200 p-4 hover:-translate-x-2 transition-all duration-300">
+            <IoArrowBackSharp className="w-7 h-7 text-rh-primarygrey" />
+          </Link>
+          <h3 className="text-3xl font-medium text-rh-primarygrey">
+            Lista de Departamentos
+          </h3>
+        </div>
+
+        <div className="flex items-center px-3 py-1.5 border border-gray-600 rounded-lg text-sm w-72 gap-3">
+          
+          <input
+            type="text"
+            placeholder="Buscar departamento"
+            className="bg-transparent flex-1 outline-none border-0 p-0 text-sm focus:ring-0"
+            onChange={(e) => setBusca(e.target.value)}
+            autoComplete="off"
+            value={busca}
+          />
+          <Search className="size-4 text-rh-primarygrey" />
+        </div>
       </div>
+
       <div className="flex flex-wrap gap-4">
         {
-          departamento.length === 0 && (
+          departamentosFiltrados.length === 0 ? (
             <div className="flex flex-col items-center justify-center w-full h-full">
-              <p className="text-xl font-semibold text-rh-primarygrey mt-4">Nenhum cargo cadastrado.</p>
+              <p className="text-xl font-semibold text-rh-primarygrey mt-4">Nenhum departamento encontrado.</p>
             </div>
+          ) : (
+            departamentosFiltrados.map((departamento) => (
+              <CardDepartamento
+                key={departamento.id}
+                departamento={departamento}
+              />
+            ))
           )
         }
-        {departamento.map((departamento) => (
-          <CardDepartamento
-            key={departamento.id}
-            departamento={departamento}
-          />
-        ))}
       </div>
     </div>
   )
