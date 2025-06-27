b53e9659388af909697a900cdda2f5b5d30893f4
wdwf <w_dwf@hotmail.com>
2025-06-27 14:17:12 -0300

    Fix optional chaining for user photo in TabelaGerencia component

--- src/pages/tabelaGerencia/TabelaGerencia.tsx
+++ src/pages/tabelaGerencia/TabelaGerencia.tsx
@@ -420,7 +420,7 @@ export default function TabelaGerencia() {
             return (
               <TableRow key={user.id}>
                 <TableCell className="flex gap-3 items-center">
-                  <img src={user.foto.length > 10 ? user.foto : noPicture} alt="foto do usuario" className="w-9 h-9 border-2 border-gray-700 rounded-full" />
+                  <img src={user.foto?.length > 10 ? user.foto : noPicture} alt="foto do usuario" className="w-9 h-9 border-2 border-gray-700 rounded-full" />
                   <div className='flex flex-col gap-1'>
                     <span className='font-semibold text-whitek'>
                       {user.nome}
